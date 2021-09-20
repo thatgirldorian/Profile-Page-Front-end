d134dd696923f76cbdd5e4111805dbd82a7fbd83
Deborah Otuagomah <debbie.otua@gmail.com>
2021-09-20 23:35:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
 function changeName() {
     document.getElementById("user-name").innerHTML = "Chinwe Kalu";
 }
+
+//function to remove user from request list
+function removeRequest() {
+    var removeButton = document.getElementById("req-btn");
+    removeButton.classList.toggle("hidden");
+}
+
+function removeRequest2() {
+    var removeButton = document.getElementById("req-btn2");
+    removeButton.classList.toggle("hidden");
+}
